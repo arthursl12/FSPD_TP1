2a6216a840b20bddd4db3b6e32ac94fc6f43e98e
arthursl12 <arthursoutolima@gmail.com>
2021-11-27 10:14:21 -0300

    Base test for queue type

--- tests/test_1.cpp
+++ tests/test_1.cpp
@@ -3,6 +3,13 @@
 
 #include "mandel-tiles-graphic.h"
 
+#include <queue>
+#include <memory>
+
+#define QUEUE_FULL_TYPE std::queue<std::shared_ptr<fractal_param_t>>
+
 TEST_CASE("Fractal0"){
-    CHECK_NOTHROW(1 == 1);
+    std::shared_ptr<QUEUE_FULL_TYPE> myQueue;
+    myQueue = std::make_shared<QUEUE_FULL_TYPE>();
+    CHECK(myQueue->empty());
 }
